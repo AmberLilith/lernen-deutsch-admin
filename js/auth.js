a94ff8bde60af83e3d7e7483f2245f4c180f62a1
Amber Lilith O. da Silva <66691730+AmberLilith@users.noreply.github.com>
2026-09-19 09:00:29 -0300

    Atualiza versão dos módulos para exclusão

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-import { auth } from "./firebase.js?v=20260919-4";
+import { auth } from "./firebase.js?v=20260919-5";
 import {
     signInWithEmailAndPassword,
     signOut,
@@ -7,7 +7,7 @@ import {
 import {
     iniciarListaSubstantivos,
     pararListaSubstantivos
-} from "./substantivos.js?v=20260919-4";
+} from "./substantivos.js?v=20260919-5";
 
 const ADMIN_UID = "IBFeuoYBZlUTGYc3BLW7Dz7lxzx2";
 
