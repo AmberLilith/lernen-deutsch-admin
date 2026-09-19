77091e2f850b26a3c412f3945b1645b867772c3a
Amber Lilith O. da Silva <66691730+AmberLilith@users.noreply.github.com>
2026-09-19 01:26:35 -0300

    Configura Firebase no painel admin

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCpjitabGljJDWnDxvu5y6l4CuNqe6ovQE",
+    authDomain: "lernen-deutsch-38102.firebaseapp.com",
+    databaseURL: "https://lernen-deutsch-38102-default-rtdb.firebaseio.com",
+    projectId: "lernen-deutsch-38102",
+    storageBucket: "lernen-deutsch-38102.firebasestorage.app",
+    messagingSenderId: "1062030106036",
+    appId: "1:1062030106036:web:b545aac2e79fed31fca6e1",
+    measurementId: "G-11BZ1C0L6R"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const database = getDatabase(app);
