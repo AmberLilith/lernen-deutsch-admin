24ac1e0ca97155374b6aacc80e1dce372d62d3d8
Amber Lilith O. da Silva <66691730+AmberLilith@users.noreply.github.com>
2026-09-19 01:35:58 -0300

    Evita cache antigo no módulo de autenticação

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-import { auth } from "./firebase.js";
+import { auth } from "./firebase.js?v=20260919-1";
 import {
     signInWithEmailAndPassword,
     signOut,
@@ -7,7 +7,7 @@ import {
 import {
     iniciarListaSubstantivos,
     pararListaSubstantivos
-} from "./substantivos.js";
+} from "./substantivos.js?v=20260919-1";
 
 const ADMIN_UID = "IBFeuoYBZlUTGYc3BLW7Dz7lxzx2";
 
